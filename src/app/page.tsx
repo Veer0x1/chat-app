e978011762fd406ee385ab38cf7b741cd28aeb6b
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2023-04-19 12:12:02 +0530

    custom button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
-
+import Button from "@/components/ui/Button";
 export default function Home() {
   return (
-    <h1  className='text-red-300'>Hello world</h1>
-  )
+    <div>
+      <h1 className="text-red-300">Hello world</h1>
+      <Button>Click</Button>
+    </div>
+  );
 }
