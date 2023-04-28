61ad7277ddc141537688453d05dd1190debff9b5
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2023-04-29 01:26:28 +0530

    fixed typescript error

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,15 @@
-import { Redis } from '@upstash/redis';
+import { Redis } from "@upstash/redis";
 
+interface EnvType {
+  UPSTASH_REDIS_REST_URL: string;
+  UPSTASH_REDIS_REST_TOKEN: string;
+}
+
+const env: EnvType = {
+  UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL!,
+  UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN!,
+};
 export const db = new Redis({
-    url: process.env.UPSTASH_REDIS_REST_URL,
-    token: process.env.UPSTASH_REDIS_REST_TOKEN,
-})
+  url: env.UPSTASH_REDIS_REST_URL,
+  token: env.UPSTASH_REDIS_REST_TOKEN,
+});
